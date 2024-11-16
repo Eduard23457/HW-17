61ef9ba68fd789282cfd283d99fd96d5e7e8cf67
Эдуард <khidiyatov2000@bk.ru>
2024-11-16 13:50:13 +0500

    Maven и Spring Boo

---
+++ src/main/java/ru/skypro/first_spring_application/service/impl/CalculatorServiceImpl.java
@@ -0,0 +1,27 @@
+package ru.skypro.first_spring_application.service.impl;
+
+import org.springframework.stereotype.Service;
+import ru.skypro.first_spring_application.service.CalculatorService;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+    @Override
+    public int plus(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public int minus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    @Override
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+}
